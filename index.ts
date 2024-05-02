9d189d07f195e002f71fb1b3dc9ab3f8ff1f3561
Luiz Calaça <luizcalaca@gmail.com>
2024-05-02 17:50:11 -0300

    style: deleting unused code

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
 import serverless from "serverless-http";
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response} from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
+app.get("/", (req: Request, res: Response) => {
   return res.status(200).json({
     message: "Hello from root!",
   });
 });
 
-app.get("/path", (req: Request, res: Response, next: NextFunction) => {
+app.get("/path", (req: Request, res: Response) => {
   return res.status(200).json({
     message: "Hello from path!",
   });
 });
 
-app.use((req: Request, res: Response, next: NextFunction) => {
+app.use((req: Request, res: Response) => {
   return res.status(404).json({
     error: "Not Found",
   });
